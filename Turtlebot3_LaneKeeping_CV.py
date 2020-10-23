2fded506139d6639f2d9e558f988d8cbbe5f5dad
inmok <ko3036@gmail.com>
2020-10-23 16:02:00 +0900

    Update Turtlebot3_LaneKeeping_CV.py

--- Turtlebot3_LaneKeeping_CV.py
+++ Turtlebot3_LaneKeeping_CV.py
@@ -15,7 +15,7 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 global LSD
 global cap
-LSD = cv2.createLineSegmentDetector(0)
+LSD = cv2.createLineSegmentDetector(0)  #이미지에서 사각형 선을 인식하는 알고리즘
 cap = cv2.VideoCapture(0)
 ''' class '''
 class robot():
@@ -25,32 +25,32 @@ class robot():
 
     def keeping(self,hsv):
         global LSD
-        vel_msg=Twist()
+        vel_msg=Twist()   # linear, angular 사용하기 위한 선언
         crop_L=hsv[350:410,40:220]
         crop_R=hsv[350:410,402:582]
-        L_mask = cv2.inRange(crop_L,(21,50,100),(36,255,255))
+        L_mask = cv2.inRange(crop_L,(21,50,100),(36,255,255)) # inRange(hsv,Low,High) : 추출하고자 하는 색이 Low~High범위 안이면 흰색 return 범위 밖이면 검은색으로 return
         L2_mask = cv2.inRange(crop_L,(36,0,165),(255,255,255))
         R_mask = cv2.inRange(crop_R,(36,0,165),(255,255,255))
         R2_mask = cv2.inRange(crop_R,(21,50,100),(36,255,255))
       
-        yello_line = LSD.detect(L_mask)
+        yello_line = LSD.detect(L_mask)   # .detect() : input값에서 line을 찾아준다
         yello_line2 = LSD.detect(L2_mask)
         white_line = LSD.detect(R_mask)
         white_line2 = LSD.detect(R2_mask)
-        if yello_line[0] is None and yello_line2[0] is None:
-            vel_msg.linear.x = 0.05
-            vel_msg.angular.z = 0.6
-        elif white_line[0] is None and white_line2[0] is None:
+        if yello_line[0] is None and yello_line2[0] is None:  # 노란색선이 인식되지 않으면
+            vel_msg.linear.x = 0.05   # linear.x : 직선속도
+            vel_msg.angular.z = 0.6   # angular.z : 회전속도
+        elif white_line[0] is None and white_line2[0] is None:   # 하얀색선이 인식되지 않으면
             vel_msg.linear.x = 0.05
             vel_msg.angular.z = -0.6
-        else :
+        else :    # 그 외 경우 = 직진
             vel_msg.linear.x = 0.2
             vel_msg.angular.z = 0
-        self.velocity_publisher.publish(vel_msg)
+        self.velocity_publisher.publish(vel_msg) # cmd_vel에 vel_msg값을 전달
 
     def imageupdate(self):
         global cap
-        ret, image=cap.read()
+        ret, image=cap.read() # image값에 읽은 프레임이 저장, ret에는 프레임읽기에 성공하면 True return
         hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
         return image,hsv
         
